63b82e12608a3e792300eddcd777cb0e6d9c02ba
Sam <samtom@ksu.edu>
2021-09-22 23:51:43 -0500

    Made changes to show largest value and its location

--- first_largest.py
+++ first_largest.py
@@ -5,11 +5,18 @@ Python program to find the largest element and its location.
 def largest_element(a):
     """ Return the largest element of a sequence a.
     """
-
-    return None
-
+    maxval = a[0]
+    maxloc = 0
+    for i in range(1, len(a)):
+        if a[i] > maxval:
+            maxval = a[i]
+            maxloc = i
+    if locals == True:
+        return maxval, maxloc
+    return maxval, maxloc   
 
 if __name__ == "__main__":
 
-    a = [1,2,3,2,1]
+    a = [1,1,1,3,5]
     print("Largest element is {:}".format(largest_element(a)))
+
